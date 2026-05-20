da01db5eb7efb26061445c5c2f8b7c5b1054aea2
PhilippeDi4s <lisboa.philippes@gmail.com>
2026-05-19 21:25:31 -0300

    implement theme toggle functionality and add theme styles

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@ const contadorElement = document.getElementById("contador_value");
 const nextButton = document.getElementById("next_button");
 const previousButton = document.getElementById("previous_button");
 
+let pageTheme = document.documentElement.dataset.theme;
+const themeButton = document.getElementById("themeButton");
+
 let contadorValue = 1;
 
 contadorElement.innerText = `${contadorValue}`;
@@ -14,4 +17,15 @@ nextButton.addEventListener("click", () => {
 previousButton.addEventListener("click", () => {
   contadorValue -= 1;
   contadorElement.innerText = `${contadorValue}`;
+});
+
+const themeIcons = {
+  dark: "<ion-icon name='sunny-outline'></ion-icon>",
+  light: "<ion-icon name='moon-outline'></ion-icon>",
+};
+
+themeButton.addEventListener("click", () => {
+  pageTheme = pageTheme === "light" ? "dark" : "light";
+  document.documentElement.dataset.theme = pageTheme;
+  themeButton.innerHTML = themeIcons[`${pageTheme}`];
 });
